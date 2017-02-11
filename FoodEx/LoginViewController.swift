970a01d8b228e2ef8ca6b13595c23defab1c9936
Nana <narayanan_vm@apple.com>
2017-02-11 15:34:07 -0800

    Implemented IBOutlets and IBActions for performing TouchID and Regular Login Authentication.

--- FoodEx/LoginViewController.swift
+++ FoodEx/LoginViewController.swift
@@ -11,28 +11,26 @@ import LocalAuthentication
 
 class LoginViewController: UIViewController {
 
+    // MARK: Properties
+    let laContext = LAContext()
+
+    // MARK: IBOutlets
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+
+    // MARK: Function Overrides
+
+    // Evaluates and presents Touch ID Auth if device and user setup supports it.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let myContext = LAContext()
         var authError : NSError? = nil
 
-        if (myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError)) {
+        if (laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError)) {
 
-            myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Sign in with Touch ID or cancel to enter password.") { (success, evaluateError) in
-
-                if (success) {
-
-                    print("Touch ID success")
-                    // Proceed displaying application dashboard
-
-                } else {
-
-                    print("Touch ID failure: \(evaluateError)")
-                    self.handleTouchIDAuthFailure(LAError(_nsError: evaluateError as! NSError).code)
-                }
-            }
+            performTouchIDAuth(nil)
 
         } else {
 
@@ -40,43 +38,100 @@ class LoginViewController: UIViewController {
         }
     }
 
+    // Memory Management
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: IBActions
+
+    // Kick starts Touch ID Auth
+    @IBAction func performTouchIDAuth(_ sender: UIButton?) {
+
+        laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Sign in with Touch ID or cancel to enter password.") { (success, evaluateError) in
+
+            if (success) {
+
+                print("Touch ID success")
+                // Proceed displaying application dashboard
+
+            } else {
+
+                print("Touch ID failure: \(evaluateError)")
+                self.handleTouchIDAuthFailure(LAError(_nsError: evaluateError as! NSError).code)
+            }
+        }
+    }
+
+    // Regular Login Authentication
+    @IBAction func performRegularAuth(_ sender: UIButton) {
+
+        var success = false
+        let userName = userNameField.text
+        let password = passwordField.text
+
+        // Validate the login credentials against registered users data mart
+
+        if (success) {
+            // Proceed displaying application dashboard
+
+        } else {
+
+            let alert = UIAlertController(title: "User Authentication Error", message: "Incorrect Username or Password. Please try again.", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+
+            present(alert, animated: true)
+        }
+    }
+
     // MARK: Utils Functions
 
+    // Handles the Touch ID Auth failure and decides to alert the user when required
     func handleTouchIDAuthFailure(_ errorCd: LAError.Code) -> Void {
 
         var userErrorInfo = ""
+        var shouldDisplayAlert = false
 
         switch errorCd {
 
         case .authenticationFailed:
-            userErrorInfo = "Touch ID Auth failed."
+            userErrorInfo = "Touch ID Auth failed. Please enter valid credentials to sign in."
+            shouldDisplayAlert = true
+
         case .userCancel:
             userErrorInfo = "Touch ID Auth cancelled by user."
+
         case .userFallback:
             userErrorInfo = "Touch ID Auth ignored and user fallsback to manual sign in."
+
         case .systemCancel:
             userErrorInfo = "Touch ID Auth cancelled by system."
+
         case .passcodeNotSet:
-            userErrorInfo = "Touch ID Auth failed as passcode was not set in the device."
+            userErrorInfo = "Touch ID Auth failed as passcode was not set in the device. Please enter valid credentials to sign in."
+            shouldDisplayAlert = true
+
         case .touchIDNotAvailable:
             userErrorInfo = "Touch ID Auth not supported in the device."
+
         case .touchIDNotEnrolled:
             userErrorInfo = "Touch ID Auth not enrolled in the device."
+
         default:
-            userErrorInfo = "Touch ID Auth failed due to unknown reasons."
+            userErrorInfo = "Touch ID Auth failed due to unknown reasons. Please enter valid credentials to sign in."
+            shouldDisplayAlert = true
         }
 
-        let alert = UIAlertController(title: "User Authentication Error", message: userErrorInfo, preferredStyle: .alert)
+        if shouldDisplayAlert {
 
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
+            let alert = UIAlertController(title: "User Authentication Error", message: userErrorInfo, preferredStyle: .alert)
 
-        present(alert, animated: true)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
 
+            present(alert, animated: true)
+        }
     }
 
 }
